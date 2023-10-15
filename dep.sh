6aee99ab529d33f998723fd05011cbacdd910b7d
Tetsuya Yamamoto <82134549+ty-foobar@users.noreply.github.com>
2023-10-15 18:50:59 +0900

    refactors dep.sh

--- dep.sh
+++ dep.sh
@@ -18,21 +18,18 @@ fi
 depFile="$1"
 > "${depFile}" # clear contents of file
 for plotFile in $(ls "${figDir}" | grep ".${plotFileExt}"); do
-    baseName="${plotFile%${plotFileExt}}" # Bash string manipulation
-
-    # suppress newline with -n
-    echo -n "${figDir}/${baseName}${figFileExt}: " >> "${depFile}"
-
     # extended regex with -E; only print matching with -o; matches "hoge.dat" or 'hoge.dat'
     dataFiles=$(grep -Eo "[\"'][^\"']+\.${dataFileExt}[\"']" "${figDir}/${plotFile}")
 
     # remove quotation marks with sed; remove duplicates with sort -u
     dataFiles=$(echo "${dataFiles}" | sed "s/[\"']//g" | sort -u)
 
-    strToWrite="${figDir}/${plotFile}"
+    prereq="${figDir}/${plotFile}"
     for dataFile in ${dataFiles}; do
-        strToWrite="${strToWrite} ${figDir}/${dataFile}"
+        prereq="${prereq} ${figDir}/${dataFile}"
     done
-    echo "${strToWrite}" >> "${depFile}"
+    baseName="${plotFile%${plotFileExt}}" # Bash string manipulation
+    target="${figDir}/${baseName}${figFileExt}"
+    echo "${target}: ${prereq}" >> "${depFile}"
 done
 echo "Wrote: ${depFile}"
